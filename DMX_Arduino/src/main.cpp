4c2b9ab236c64e4b40f86ca469b6721bacdb74ee
F. St <39712181+Feriixu@users.noreply.github.com>
2022-10-17 13:42:28 +0200

    Update DMX Arduino to use new command parsing structure

--- DMX_Arduino/src/main.cpp
+++ DMX_Arduino/src/main.cpp
@@ -6,16 +6,10 @@
 #define d_BAD_SYNTAX            ("BAD_SYNTAX\n")
 #define d_UNRECOGNIZED_COMMAND  ("UNRECOGNIZED_COMMAND\n")
 
-enum Command {
-    ID,
-    TRIGGER,
-    UNRECOGNIZED
-};
-
-Command get_command();
-
 void cmd_id();
+
 void cmd_trigger();
+
 void cmd_unrecognized();
 
 // Parser defines
@@ -25,69 +19,41 @@ void cmd_unrecognized();
 // Communication defines
 #define d_BAUD_RATE             (115200)
 
-char mca_StringBuffer[d_MAX_STRING_SIZE] = {0}; // Read bytes
-int mc_ReadBytes = 0; // Amount of read bytes
-int incomingByte = 0; // For incoming serial data
-
-void setup ()
-{
-    Serial.begin (d_BAUD_RATE);
+void setup() {
+    Serial.begin(d_BAUD_RATE);
 }
 
-void loop ()
-{
-    // Read until \n
-    while (true) {
-        // Wait until bytes available
-        while (Serial.available() == 0);
-        // Read byte
-        incomingByte = Serial.read();
-
-        // If it is a \n stop reading
-        if (incomingByte == '\n') {
-            break;
-        } else {
-            // Otherwise, add char to buffer
-            mca_StringBuffer[mc_ReadBytes] = (char) incomingByte;
-            mc_ReadBytes += 1;
-        }
-    }
-
-    // Handle command
-    switch (get_command()) {
-        case Command::ID:
-            cmd_id();
-        case Command::TRIGGER:
-            cmd_trigger();
-        case Command::UNRECOGNIZED:
-            cmd_unrecognized();
-    }
-
-    // Cleanup
-    mc_ReadBytes = 0;
-    memset(&mca_StringBuffer, 0, d_MAX_STRING_SIZE);
-}
-
-Command get_command() {
-    if (strncmp("?", mca_StringBuffer, 1) == 0) {
-        return Command::ID;
-    } else if (strncmp("TRIGGER", mca_StringBuffer, 7) == 0) {
-        return Command::TRIGGER;
+void process_data(char *data) {
+    if (strncmp("?", data, 1) == 0) {
+        Serial.print(d_ID);
+    } else if (strncmp("TRIGGER", data, 7) == 0) {
+        // TODO: Trigger firework here
+        Serial.print(d_OK);
     } else {
-        return Command::UNRECOGNIZED;
+        Serial.print(d_UNRECOGNIZED_COMMAND);
     }
-}
+}  // end of process_data
 
-void cmd_id() {
-    Serial.print(d_ID);
-}
+void processIncomingByte(const byte inByte) {
+    static char input_line[d_MAX_STRING_SIZE];
+    static unsigned int input_pos = 0;
 
-void cmd_trigger() {
-    // TODO: Feuerwerk auslösen
-    
-    Serial.print(d_OK);
-}
+    switch (inByte) {
+        case '\n':   // end of text
+            input_line[input_pos] = 0;  // terminating null byte
+            process_data(input_line);
+            input_pos = 0;
+            break;
+        case '\r':   // discard carriage return
+            break;
+        default:
+            if (input_pos < (d_MAX_STRING_SIZE - 1))
+                input_line[input_pos++] = inByte;
+            break;
+    }  // end of switch
+} // end of processIncomingByte
 
-void cmd_unrecognized() {
-    Serial.print(d_UNRECOGNIZED_COMMAND);
+void loop() {
+    while (Serial.available() > 0)
+        processIncomingByte(Serial.read());
 }
