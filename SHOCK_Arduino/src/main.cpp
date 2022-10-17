2c33a8e4ba0b710dee22e0949ad13a48772454f6
F. St <39712181+Feriixu@users.noreply.github.com>
2022-10-17 14:17:59 +0200

    Use new parsing structure

--- SHOCK_Arduino/src/main.cpp
+++ SHOCK_Arduino/src/main.cpp
@@ -30,12 +30,7 @@ enum Level {
 
 ShockPad shock_pads[4] = {DONT_SHOCK, DONT_SHOCK, DONT_SHOCK, DONT_SHOCK};
 Level shock_level = Level1;
-bool parse_set();
-
-void cmd_id();
-void cmd_set();
-void cmd_play();
-void cmd_unrecognized();
+bool parse_set(char *string);
 
 // Parser defines
 #define d_MAX_STRING_SIZE       (64)
@@ -43,102 +38,65 @@ void cmd_unrecognized();
 // Communication defines
 #define d_BAUD_RATE             (115200)
 
-char mca_StringBuffer[d_MAX_STRING_SIZE] = {0}; // Read bytes
-int mc_ReadBytes = 0; // Amount of read bytes
-int incomingByte = 0; // For incoming serial data
-
 void setup ()
 {
     Serial.begin (d_BAUD_RATE);
 }
 
+void process_data (char * data)
+{
+    if (strncmp("?", data, 1) == 0) {
+        Serial.print(d_ID);
+    } else if (strncmp("SET", data, 3) == 0) {
+        if (parse_set(data)) {
+            Serial.print(d_OK);
+        } else {
+            Serial.print(d_BAD_SYNTAX);
+        }
+    } else if (strncmp("PLAY", data, 4) == 0) {
+        char p1, p2, p3, p4;
+        // TODO: Pads in shock_pads mit level shock_level schocken
+
+        char response[21];
+        sprintf(response, "P1=%d;P2=%d;P3=%d;P4=%d\n", p1, p2, p3, p4);
+        Serial.print(response);
+    } else {
+        Serial.print(d_UNRECOGNIZED_COMMAND);
+    }
+}  // end of process_data
+
+void processIncomingByte (const byte inByte)
+{
+    static char input_line [d_MAX_STRING_SIZE];
+    static unsigned int input_pos = 0;
+    switch (inByte)
+    {
+        case '\n':   // end of text
+            input_line [input_pos] = 0;  // terminating null byte
+            process_data (input_line);
+            input_pos = 0;
+            break;
+        case '\r':   // discard carriage return
+            break;
+        default:
+            // keep adding if not full ... allow for terminating null byte
+            if (input_pos < (d_MAX_STRING_SIZE - 1))
+                input_line [input_pos++] = inByte;
+            break;
+    }  // end of switch
+} // end of processIncomingByte
+
 void loop ()
 {
-    // Read until \n
-    while (true) {
-        // Wait until bytes available
-        while (Serial.available() == 0);
-        // Read byte
-        incomingByte = Serial.read();
-
-        // If it is a \n stop reading
-        if (incomingByte == '\n') {
-            break;
-        } else {
-            // Otherwise, add char to buffer
-            mca_StringBuffer[mc_ReadBytes] = (char) incomingByte;
-            mc_ReadBytes += 1;
-        }
-    }
-
-    // Handle command
-    switch (get_command()) {
-        case Command::ID:
-            cmd_id();
-            break;
-        case Command::SET:
-            cmd_set();
-            break;
-        case Command::PLAY:
-            cmd_play();
-            break;
-        case Command::UNRECOGNIZED:
-            cmd_unrecognized();
-            break;
-    }
-
-    // Cleanup
-    mc_ReadBytes = 0;
-    memset(&mca_StringBuffer, 0, d_MAX_STRING_SIZE);
+    while (Serial.available () > 0)
+        processIncomingByte (Serial.read ());
 }
 
-Command get_command() {
-    if (strncmp("?", mca_StringBuffer, 1) == 0) {
-        return Command::ID;
-    } else if (strncmp("SET", mca_StringBuffer, 3) == 0) {
-        return Command::SET;
-    } else if (strncmp("PLAY", mca_StringBuffer, 4) == 0) {
-        return Command::PLAY;
-    } else {
-        return Command::UNRECOGNIZED;
-    }
-}
-
-void cmd_id() {
-    Serial.print(d_ID);
-}
-
-void cmd_set() {
-    if (parse_set()) {
-        Serial.print(d_OK);
-    } else {
-        Serial.print(d_BAD_SYNTAX);
-    }
-
-}
-
-void cmd_play() {
-    char p1, p2, p3, p4;
-    // TODO: Pads in shock_pads mit level shock_level schocken
-
-
-    char response[21];
-    sprintf(response, "P1=%d;P2=%d;P3=%d;P4=%d\n", p1, p2, p3, p4);
-    Serial.print(response);
-}
-
-void cmd_unrecognized() {
-    Serial.print(d_UNRECOGNIZED_COMMAND);
-}
-
-/// Parses a SET command and saves the switch states in switches.
-/// \param switches - Should be an int[5], initialized with -1;
-/// \return True, when no parsing errors.
-bool parse_set() {
+bool parse_set(char *data) {
     Level _shock_level = InvalidLevel;
     ShockPad _shock_pads[4] = { DONT_SHOCK, DONT_SHOCK, DONT_SHOCK, DONT_SHOCK };
 
-    strtok(mca_StringBuffer, ":"); // Skip command part
+    strtok(data, ":"); // Skip command part
     char* argument = strtok(nullptr, ";"); // Get next argument
     while (argument != nullptr) {
         _shock_level = InvalidLevel;
