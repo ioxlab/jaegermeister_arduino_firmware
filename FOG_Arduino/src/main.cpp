0fd7526e4128442b939a8a3be353dd6d086fbd22
F. St <39712181+Feriixu@users.noreply.github.com>
2022-10-17 14:17:38 +0200

    Use new parsing structure

--- FOG_Arduino/src/main.cpp
+++ FOG_Arduino/src/main.cpp
@@ -8,27 +8,11 @@
 // Fog machine
 #define PIN_FOG 13
 
-enum Command {
-    ID,
-    TRIGGER,
-    UNRECOGNIZED
-};
-
-Command get_command();
-
-void cmd_id();
-void cmd_trigger();
-void cmd_unrecognized();
-
 // Parser defines
 #define d_MAX_STRING_SIZE       (64)
 
 // Communication defines
 #define d_BAUD_RATE             (115200)
-
-char mca_StringBuffer[d_MAX_STRING_SIZE] = {0}; // Read bytes
-int mc_ReadBytes = 0; // Amount of read bytes
-int incomingByte = 0; // For incoming serial data
 
 void setup ()
 {
@@ -36,61 +20,45 @@ void setup ()
     Serial.begin (d_BAUD_RATE);
 }
 
+// here to process incoming serial data after a terminator received
+void process_data (char * data)
+{
+    if (strncmp("?", data, 1) == 0) {
+        Serial.print(d_ID);
+    } else if (strncmp("TRIGGER", data, 7) == 0) {
+        digitalWrite(PIN_FOG, HIGH);
+        delay(500);
+        digitalWrite(PIN_FOG, LOW);
+        Serial.print(d_OK);
+    } else {
+        Serial.print(d_UNRECOGNIZED_COMMAND);
+    }
+}  // end of process_data
+
+void processIncomingByte (const byte inByte)
+{
+    static char input_line [d_MAX_STRING_SIZE];
+    static unsigned int input_pos = 0;
+
+    switch (inByte)
+    {
+        case '\n':   // end of text
+            input_line [input_pos] = 0;  // terminating null byte
+            process_data (input_line);
+            input_pos = 0;
+            break;
+        case '\r':   // discard carriage return
+            break;
+        default:
+            // keep adding if not full ... allow for terminating null byte
+            if (input_pos < (d_MAX_STRING_SIZE - 1))
+                input_line [input_pos++] = inByte;
+            break;
+    }  // end of switch
+} // end of processIncomingByte
+
 void loop ()
 {
-    // Read until \n
-    while (true) {
-        // Wait until bytes available
-        while (Serial.available() == 0);
-        // Read byte
-        incomingByte = Serial.read();
-
-        // If it is a \n stop reading
-        if (incomingByte == '\n') {
-            break;
-        } else {
-            // Otherwise, add char to buffer
-            mca_StringBuffer[mc_ReadBytes] = (char) incomingByte;
-            mc_ReadBytes += 1;
-        }
-    }
-
-    // Handle command
-    switch (get_command()) {
-        case Command::ID:
-            cmd_id();
-        case Command::TRIGGER:
-            cmd_trigger();
-        case Command::UNRECOGNIZED:
-            cmd_unrecognized();
-    }
-
-    // Cleanup
-    mc_ReadBytes = 0;
-    memset(&mca_StringBuffer, 0, d_MAX_STRING_SIZE);
-}
-
-Command get_command() {
-    if (strncmp("?", mca_StringBuffer, 1) == 0) {
-        return Command::ID;
-    } else if (strncmp("TRIGGER", mca_StringBuffer, 7) == 0) {
-        return Command::TRIGGER;
-    } else {
-        return Command::UNRECOGNIZED;
-    }
-}
-
-void cmd_id() {
-    Serial.print(d_ID);
-}
-
-void cmd_trigger() {
-    digitalWrite(PIN_FOG, HIGH);
-    delay(500);
-    digitalWrite(PIN_FOG, LOW);
-    Serial.print(d_OK);
-}
-
-void cmd_unrecognized() {
-    Serial.print(d_UNRECOGNIZED_COMMAND);
+    while (Serial.available () > 0)
+        processIncomingByte (Serial.read ());
 }
